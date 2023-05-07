44ceb2e982473b0d206b9e80c0ae82e6b9a34dfb
ValeriiSanduliak <112845911+ValeriiSanduliak@users.noreply.github.com>
2023-05-07 17:46:36 +0300

    Add files

--- OOPLab7T/OOPLab7T.cpp
+++ OOPLab7T/OOPLab7T.cpp
@@ -1,11 +1,9 @@
 ﻿#include <iostream>
 #include <clocale>
-
+#include <fstream>
+using namespace std;
 int main()
 {
-    setlocale(LC_CTYPE, "ukr");
-    std::cout << "Лабораторна робота №7  \n";
-    std::cout << "Автоматизована система ...  \n";
-    std::cout << "Hello World!";
+    
 }
 
